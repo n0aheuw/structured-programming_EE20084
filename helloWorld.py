59e563e43029d593c3b9f3332e6716507e2244d5
no0n00 <119894880+no0n00@users.noreply.github.com>
2023-02-09 11:07:51 +0000

    Delete helloWorld.py

--- helloWorld.py
+++
@@ -1,8 +0,0 @@
-###################################################################################################
-#   Filename:       helloWorld.py
-#   Description:    A simple hello world example. Just to show the process for python development.
-#   Date:           07/02/2023
-#   Author:         no0n00
-###################################################################################################
-
-print("hello world")
